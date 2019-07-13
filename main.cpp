496bdf53e71bf2a2a186cc93d44fe61bf913413b
mehow93 <michmad93@gmail.com'git config --global user.email michmad93@gmail.comdsfbvsecho penisgit config --global user.email "michmad93@gmail.com>
2019-07-13 11:27:51 +0200

    def. of class BST

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+class BTS
+{
+public:
+
+
+
+
+private:
+    struct Node
+    {
+        Node(int val, Node *ls, Node *rs) : data{val}, pLeft{ls}, pRight{rs}{}
+        ~Node() { cout << "Deletion of: " << this->data << endl; }
+        int data;
+        Node *pLeft;
+        Node *pRight;
+    };
+};
 int main()
 {
     cout << "Hello World!" << endl;
