f0c15fb680be0ec1c61641fc3a3236443a4201f8
mehow93 <michmad93@gmail.com'git config --global user.email michmad93@gmail.comdsfbvsecho penisgit config --global user.email "michmad93@gmail.com>
2019-07-13 13:01:18 +0200

    addind nodes done

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class BTS
 {
 public:
+    BTS& insert_node(int var);
 
 
 
@@ -18,7 +19,38 @@ private:
         Node *pLeft;
         Node *pRight;
     };
+
+    Node *pRoot_;
+    Node* insert_as_subtree(Node *node, int var);
 };
+
+BTS& BTS::insert_node(int var) // insert root or go insert subtree
+{
+    if(pRoot_ == nullptr)
+        pRoot_ = new Node(var,nullptr,nullptr);
+
+    return *this;
+}
+BTS::Node* BTS::insert_as_subtree(Node *node, int var)// create subtree
+{
+    if(node == nullptr)
+        return new Node(var,nullptr,nullptr);
+    else
+    {
+        if(var <= node->data)
+        {
+            node->pLeft = insert_as_subtree(node->pLeft,var);
+            return node;
+        }
+        else
+        {
+            node->pRight = insert_as_subtree(node->pRight,var);
+            return node;
+        }
+
+     }
+
+}
 int main()
 {
     cout << "Hello World!" << endl;
